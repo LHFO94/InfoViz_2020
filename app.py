1cb91c01bd33f530f505bff2f47da2881d58d6db
JoshuaBruens <josh.bruens@gmail.com>
2020-03-10 17:41:46 +0100

    Trying to adapt Oscars intro page

--- app.py
+++ app.py
@@ -17,28 +17,27 @@ app.config['image_list'] = create_image_list('./static/pictures/')
 app.config['attributes'] = pd.read_csv("./static/attributes/NOWHERE_DATASET.csv", header=[0, 1], index_col=0)
 app.config['data'] =pd.read_csv("./static/attributes/results_TSNE_2d.csv", header=[0])
 
-@app.route('/',methods=['POST','GET'])
-def index():
-    if request.method == "POST":
-        app.config['image_name'] = request.form["content"]
-        return redirect('/')
-    else:
-        image_name = app.config['image_name']
-        PEOPLE_FOLDER = os.path.join('static', 'pictures')
-        picture_filename = image_name + '.jpg'
-        full_filename = os.path.join(PEOPLE_FOLDER , picture_filename)
-        print (full_filename)
-        image_list = app.config['image_list']
-        db_attributes = app.config['attributes']
 
-        return render_template('index.html', image = image_name ,user_image = full_filename, image_list = image_list )
 
-@app.route('/graph', methods=['POST','GET'])
+@app.route('/', methods=['POST','GET'])
+def intro():
+
+    if request.method == 'POST':
+        try :
+            return  redirect('/images/')
+
+        except :
+            return "There was an issue updating your task"
+
+    else :
+        return render_template('intro_page.html')
+
+@app.route('/graph/', methods=['POST','GET'])
 def graph():
 
     if request.method == 'POST':
         try :
-            return  redirect('/')
+            return  redirect('/images/')
 
         except :
             return "There was an issue updating your task"
@@ -53,28 +52,10 @@ def graph():
         return render_template('graph.html', image_list = image_list, db_attr = db_attributes )
 
 
-
-
-
-
 @app.route('/graph/data/attributes')
 def get_d3_data():
     df = app.config['data']
     return df.to_csv()
-
-
-@app.route('/intro', methods=['POST','GET'])
-def intro():
-
-    if request.method == 'POST':
-        try :
-            return  redirect('/')
-
-        except :
-            return "There was an issue updating your task"
-
-    else :
-        return render_template('intro_page.html')
 
 
 if __name__ == "__main__":

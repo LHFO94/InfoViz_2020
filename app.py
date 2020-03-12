abd218da302d013f9c63dc4fb5f39c795bd2db69
LHFO94 <Luuk@wcw-stud-145-109-8-81.wireless.uva.nl>
2020-03-12 11:55:38 +0100

    reset

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template , url_for, request,redirect
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import pandas as pd
@@ -8,16 +8,19 @@ import os
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-def create_image_list(path) :
+
+def create_image_list(path):
     image_list = os.listdir(path)
     return image_list
 
+
 def read_csv(name):
     csv_path = "./static/attributes/csv/" + name + '.csv'
-    json_path = "./static/attributes/json/"+ name + ".json"
-    df = pd.read_csv(csv_path, sep = ';')
+    json_path = "./static/attributes/json/" + name + ".json"
+    df = pd.read_csv(csv_path, sep=';')
     df.set_index('Name', inplace=True)
-    df.to_json (json_path)
+    df.to_json(json_path)
+
 
 app = Flask(__name__)
 app.config['image_name'] = 'swan'
@@ -25,9 +28,9 @@ app.config['image_list'] = create_image_list('./static/pictures/')
 app.config['attributes'] = pd.read_csv("./static/attributes/NOWHERE_DATASET.csv", header=[0, 1], index_col=0)
 app.config['data'] = pd.read_csv("./static/attributes/results_TSNE_2d.csv", header=[0])
 
-@app.route('/',methods=['POST','GET'])
-def index():
 
+@app.route('/', methods=['POST', 'GET'])
+def index():
 
     if request.method == "POST":
         app.config['image_name'] = request.form["content"]
@@ -37,24 +40,25 @@ def index():
         image_name = app.config['image_name']
         PEOPLE_FOLDER = os.path.join('static', 'pictures')
         picture_filename = image_name + '.jpg'
-        full_filename = os.path.join(PEOPLE_FOLDER , picture_filename)
-        print (full_filename)
+        full_filename = os.path.join(PEOPLE_FOLDER, picture_filename)
+        print(full_filename)
         image_list = app.config['image_list']
         db_attributes = app.config['attributes']
 
-        return render_template('index.html', image = image_name ,user_image = full_filename, image_list = image_list )
+        return render_template('index.html', image=image_name, user_image=full_filename, image_list=image_list)
 
-@app.route('/graph/', methods=['POST','GET'])
+
+@app.route('/graph/', methods=['POST', 'GET'])
 def graph():
 
     if request.method == 'POST':
-        try :
-            return  redirect('/')
+        try:
+            return redirect('/')
 
-        except :
+        except:
             return "There was an issue updating your task"
 
-    else :
+    else:
         get_d3_data()
 
         # image_name = app.config['image_name']
@@ -64,8 +68,7 @@ def graph():
         image_list = app.config['image_list']
         db_attributes = app.config['attributes']
 
-
-        return render_template('graph.html', user_image = full_filename, image_list = image_list, db_attr = db_attributes )
+        return render_template('graph.html', user_image=full_filename, image_list=image_list, db_attr=db_attributes)
 
 
 @app.route('/graph/data/attributes')
@@ -73,19 +76,21 @@ def get_d3_data():
     df = app.config['data']
     return df.to_csv()
 
+
 @app.route('/sequence', methods=['POST', 'GET'])
 def sequence():
 
     if request.method == 'POST':
-        try :
-            return  redirect('/')
+        try:
+            return redirect('/')
 
-        except :
+        except:
             return "There was an issue updating your task"
     else:
 
         image_list = app.config['image_list']
         return render_template('index.html', image_list=image_list)
 
+
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(debug=True)

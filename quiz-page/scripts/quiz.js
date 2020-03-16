9de2e8519dcc3d22c5a8853a617d9b5e1fcd0480
flamingmelon <oscarmcginty@gmail.com>
2020-03-16 13:09:02 +0100

    Added quiz text, extra styling

--- quiz-page/scripts/quiz.js
+++ quiz-page/scripts/quiz.js
@@ -92,32 +92,148 @@ const quizContainer = document.getElementById('quiz');
 const answerSubmit = document.querySelectorAll('button');
 const questionTexts = [
     {
-        question: 'QUESTION 1',
+        question: 'A lion is missing from the circus.<br> Where was it last seen?',
         answers: {
-            a: 'ANSWER 1A',
-            b: 'ANSWER 1B',
-            c: 'ANSWER 1C'
+            a: 'At the empty parking lot.',
+            b: 'On its way home.',
+            c: 'Strapped to a remote control plane.',
+            d: 'At the circus.',
+            e: 'Chasing lawnmowers.'
         },
     },
     {
-        question: 'QUESTION 2',
+        question: 'Which has the strongest smell?',
         answers: {
-            a: 'ANSWER 2A',
-            b: 'ANSWER 2B',
-            c: 'ANSWER 2C'
+            a: 'Success.',
+            b: 'A blank canvas.',
+            c: 'Personalized marketing.',
+            d: 'Lies.',
+            e: 'Home cooking.'
         },
     },
     {
-        question: 'QUESTION 3',
+        question: 'Who just parked your car?',
         answers: {
-            a: 'ANSWER 3A',
-            b: 'ANSWER 3B',
-            c: 'ANSWER 3C'
+            a: 'I have a self-driving car.',
+            b: 'Check the security footage.',
+            c: 'The bus driver.',
+            d: 'My second husband.',
+            e: 'You mean my helicopter?'
+        }
+    },
+    {
+        question: 'What was the last thing you lost?',
+        answers: {
+            a: 'My phone.',
+            b: '10 kilos.',
+            c: 'Myself, on the dance floor.',
+            d: 'My voice.',
+            e: 'My job.'
+        }
+    },
+    {
+        question: 'What turns the streetlights on?',
+        answers: {
+            a: 'The passage of time.',
+            b: 'Nobody.',
+            c: 'Fear of the dark.',
+            d: 'Property developers.',
+            e: 'The desire to be seen.'
+        }
+    },
+    {
+        question: 'Where do you disappear?',
+        answers: {
+            a: 'In the spotlight.',
+            b: 'Under the bridge.',
+            c: 'The cinema at noon.',
+            d: 'Behind the desk.',
+            e: 'At the checkout queue.'
+        }
+    },
+    {
+        question: 'The elevator doors close.<br>You\'re with a laundry worker.<br>They ask you to be quiet.<br>You haven\'t said anything. What do you reply?',
+        answers: {
+            a: '<i>I have been silent long enough.</i>',
+            b: '<i>Next time, take the stairs.</i>',
+            c: 'Nothing.',
+            d: '<i>That suit is dry-clean only.</i>',
+            e: '<i>Where are we?</i>'
+        }
+    },
+    {
+        question: 'Who would you lose a fight to?',
+        answers: {
+            a: 'An Instagram filter.',
+            b: 'My kids.',
+            c: 'The apple that gave us gravity.',
+            d: 'The temptation of a perfect crime.',
+            e: 'Bulldozers.'
+        }
+    },
+    {
+        question: 'What do you see on your way to work?',
+        answers: {
+            a: 'Old men in new apartment blocks.',
+            b: 'The rising sun.',
+            c: 'The corridor ahead lights up.',
+            d: 'Empty shopping malls.',
+            e: 'My bad decisions.'
+        }
+    },
+    {
+        question: 'What is the greatest miracle of the 21st Century?',
+        answers: {
+            a: 'On-demand streaming.',
+            b: 'The trans-atlantic highway.',
+            c: 'Space tourism.',
+            d: 'The persistence of paper mail.',
+            e: 'Minecraft.'
+        }
+    },
+    {
+        question: 'How are you getting home?',
+        answers: {
+            a: 'I\'m already there.',
+            b: 'Ask my driver.',
+            c: 'Where\'s home?',
+            d: 'With a map.',
+            e: 'By elevator.'
+        }
+    },
+    {
+        question: 'Where do the stars shine brightest?',
+        answers: {
+            a: 'Hollywood.',
+            b: 'I have a concussion.',
+            c: 'On the American flag.',
+            d: 'In a dark room.',
+            e: 'When cars reflect the streetlights.'
+        }
+    },
+    {
+        question: 'Why do you wear hats?',
+        answers: {
+            a: 'My parents made hats.',
+            b: 'I\'ve lost my wig.',
+            c: 'Why don\'t you?',
+            d: 'A man is only as tall as his hat.',
+            e: 'I\'m not wearing anything else.'
+        }
+    },
+    {
+        question: 'You\'re walking down the pavement.<br>A familiar face sits by the cafe window.<br>It\'s a teacher from high school.<br>What happened to them?',
+        answers: {
+            a: 'Revolution.',
+            b: 'Children.',
+            c: 'The lottery.',
+            d: 'Pornography.',
+            e: 'Vietnam.'
         }
     },
     {
         question: 'END OF QUIZ',
-        answers: {}
+        answers: {a: 'CONTINUE'}
     }
 ];
 
@@ -133,6 +249,7 @@ let currentSlide = 0;
 
 // Show the first slide
 showSlide(currentSlide);
+console.log(slides.length);
 
 // Two more functions
 function showNextSlide() { // Fades out current slide, calls showSlide() for current + 1.
@@ -143,13 +260,16 @@ function showNextSlide() { // Fades out current slide, calls showSlide() for cur
         fadeout(slides[currentSlide]);
         showSlide(currentSlide + 1);
     }
+    if(answerArray.length === slides.length) {
+        console.log(JSON.stringify(answerArray));
+        }
 }
 
 function backtoStart() { // Resets current slide.
     let currentSlide = 0;
     answerArray.length = 0;
     showSlide(currentSlide, alsofade = true);
-} 
+}
 
 // Event listeners
 for (let i = 0 ; i < answerButtons.length; i++) { // Go to next slide on button click.

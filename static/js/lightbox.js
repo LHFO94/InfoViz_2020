4718bd9a4bc8a9e03fe10398882763981ee0e576
LHFO94 <dhr.l.hofman@gmail.com>
2020-03-19 16:18:32 +0100

    fixed the array

--- static/js/lightbox.js
+++ static/js/lightbox.js
@@ -1,12 +1,15 @@
+// create lightbox and append to body
 const lightbox = document.createElement('div')
 lightbox.id = 'lightbox'
 document.body.appendChild(lightbox)
 
+// create button
 const button = document.createElement('button')
 button.id = 'dislike_button'
 
-const images = document.querySelectorAll('img')
-images.forEach(image => {
+// add lightbox if clicked on image
+const js_images = document.querySelectorAll('img')
+js_images.forEach(image => {
   image.addEventListener('click', e => {
     lightbox.classList.add('active')
     const img = document.createElement('img')
@@ -18,24 +21,39 @@ images.forEach(image => {
       lightbox.removeChild(lightbox.firstChild)
     }
     lightbox.appendChild(img)
-
-    var width = - ((img.clientWidth) / 2 + 25)
     lightbox.appendChild(button)
-    button.style.left = width + 'px'
-    button.style.top = '-75px'
     button.innerHTML = 'Dislike'
     button.classList.add('active')
   })
 })
 
+// remove lightbox if clicked outside image
 lightbox.addEventListener('click', e => {
   if (e.target !== e.currentTarget) return
   lightbox.classList.remove('active')
 })
 
+// update image if dislike button is pressed
 button.addEventListener("click", function(){
+  // find current img DOM
   var img = lightbox.firstChild;
-  img.src = secondary_images[1]'
+
+  console.log(images)
+
+  // src swap
+  old_src = img.src
+  img.src = secondary_images[1]
+
+  // update primary image array
+  for(var i = 0; i < images.length; i++) {
+    console.log(images[i].slice(2))
+    if (old_src.slice(21) == images[i].slice(2)) {
+      console.log('foo')
+      images[i] = img.src.slice(21)
+    }
+  }
   lightbox.appendChild(button)
+  // remove used image from array
   secondary_images.shift()
-});
+  console.log(images)
+})
